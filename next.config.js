8464f3d59a8b07711f9a9d1fe695a04ffae77200
jhchee <cheehong05090@gmail.com>
2022-03-06 19:49:52 +0800

    update next js version

--- next.config.js
+++ next.config.js
@@ -1,19 +1,33 @@
 module.exports = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: [{loader: "@svgr/webpack", options: {
-        svgoConfig: {
-            plugins: [{removeViewBox: false}] // preserve view box
-        }
-    }}],
-    });
+  webpack(config, {isServer}) {
     if (!isServer) {
-      config.node = {
-        fs: 'empty'
-      }
+      config.resolve.fallback.fs = false
     }
-    return config;
+    config.module.rules.push({
+      test: /\.svg?$/,
+      oneOf: [
+        {
+          use: [
+            {
+              loader: '@svgr/webpack',
+              options: {
+                prettier: false,
+                svgo: true,
+                svgoConfig: {
+                  plugins: [{removeViewBox: false}],
+                },
+                titleProp: true,
+              },
+            },
+          ],
+          issuer: {
+            and: [/\.(ts|tsx|js|jsx|md|mdx)$/],
+          },
+        },
+      ],
+    })
+
+    return config
   },
   typescript: {
     // !! WARN !!
