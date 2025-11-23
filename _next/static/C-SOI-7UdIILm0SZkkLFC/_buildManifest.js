bf6e4ababfe0f65d974e04faca1dc2f773331cd6
jhchee <33974196+jhchee@users.noreply.github.com>
2025-11-23 20:42:24 +0000

    Deploying to gh-pages from @ jhchee/jhchee.github.io@2289f591692725b8e2a686d6e55eec87fe8eb326 🚀

---
+++ _next/static/C-SOI-7UdIILm0SZkkLFC/_buildManifest.js
@@ -0,0 +1,30 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/77ed2c234306389a.js"
+  ],
+  "/_error": [
+    "static/chunks/52a77b68324ffde6.js"
+  ],
+  "/blogs": [
+    "static/chunks/ac3f1c8ab7558b1c.js"
+  ],
+  "/posts/[slug]": [
+    "static/chunks/00f8de04f9e6fdb8.js"
+  ],
+  "/projects": [
+    "static/chunks/9674c3633c546c3b.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/blogs",
+    "/posts/[slug]",
+    "/projects"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/9674c3633c546c3b.js
@@ -0,0 +1,8 @@
+__turbopack_load_page_chunks__("/projects", [
+  "static/chunks/d5e418d2660f1071.js",
+  "static/chunks/37b74a240056e37f.js",
+  "static/chunks/4feaad6a4397bdea.js",
+  "static/chunks/72027cbfa35fff06.js",
+  "static/chunks/db2bfe74d307fa57.js",
+  "static/chunks/turbopack-0b566a4abf3152b2.js"
+])
